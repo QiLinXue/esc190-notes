304b124089cc72456ece6eb3b176189befd3fe8c
QiLinXue <qilin.xue@mail.utoronto.ca>
2023-03-16 00:30:19 -0400

    revert

--- data_structures/linkedlist.py
+++ data_structures/linkedlist.py
@@ -9,7 +9,6 @@ class Node:
 class LinkedList:
     def __init__(self):
         self.head = None
-        self.tail = None
     
     def get_i(self, i):
         # return the value at index i
@@ -22,13 +21,16 @@ class LinkedList:
         '''Add a new node with the value value to the end of the list'''
         # Create a new node
         new_node = Node(value)
-        
+        print(new_node)
+
         if self.head == None:
             self.head = new_node
-            self.tail = new_node
-
-        self.tail.next = new_node
-        self.tail = new_node
+        else:
+            # Find the last node
+            cur = self.head
+            while cur.next != None:
+                cur = cur.next
+            cur.next = new_node
     
     def insert(self, value, i):
         '''Insert a node with the value value at index i'''
@@ -37,16 +39,12 @@ class LinkedList:
         if i == 0:
             new_node.next = self.head
             self.head = new_node
-        
         else:
             cur = self.head
             for j in range(i-1):
                 cur = cur.next
             new_node.next = cur.next
             cur.next = new_node
-        
-        if new_node.next == None:
-            self.tail = new_node
 
     def __str__(self):
         cur = self.head
